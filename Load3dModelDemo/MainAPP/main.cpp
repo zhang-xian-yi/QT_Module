d86445b342882ee69f25f479f98f120c23d2c352
zhangxianyi <1724920805.com>
2023-03-09 17:17:36 +0800

    提交主函数--注意此时因效率问题可能需要反复多次渲染才能成功

--- Load3dModelDemo/MainAPP/main.cpp
+++ Load3dModelDemo/MainAPP/main.cpp
@@ -13,20 +13,23 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QApplication::setFont(QFont("Microsoft YaHei UI", 9));
     MainWindow w;
+    w.setGeometry(100,100,800,600);
     //窗口初始化字符才可以加载
-
+    /*
     QSharedPointer<OWlayerNS::IOpenGLWin> pWin =  OWlayerNS::OpenGLWinLayerrFactory::GetInstance().GetWindowPtr();
     pWin->initOpenGLWin(&w);
+    w.show();//先显示窗口 再加载
     QSharedPointer<OWlayerNS::ILoad3D> pLM = OWlayerNS::OpenGLWinLayerrFactory::GetInstance().GetLoad3DPtr();
     pLM->Load3DModel("D:\\3DModels\\obj\\Weapon_1.obj");
+    */
 
 
-    /*
     QSharedPointer<FERendererNS::IOpenGLWin> pWin =  FERendererNS::FERendererLayerFactory::GetInstance().GetWindowPtr();
     pWin->initOpenGLWin(&w);
+    w.show();//先显示窗口 再加载
     QSharedPointer<FERendererNS::ILoad3D> pLM = FERendererNS::FERendererLayerFactory::GetInstance().GetLoad3DPtr();
     pLM->LoadFiniteElementData("D:\\3DModels\\result_rank_3300_0.dat");
-    */
-    w.show();
+
+
     return a.exec();
 }
