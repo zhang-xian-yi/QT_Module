bc2ec0a0105aaa790075ad3d2ffc850699542633
zhangxianyi <1724920805.com>
2023-03-11 22:31:58 +0800

    初步优化--结束

--- Load3dModelDemo/FiniteElementRenderLayer/Src/FELogicService/OpenGLEntity/FEModel.cpp
+++ Load3dModelDemo/FiniteElementRenderLayer/Src/FELogicService/OpenGLEntity/FEModel.cpp
@@ -2,12 +2,8 @@
 
 FEModel::FEModel()
 {
-    m_vao = QSharedPointer<QOpenGLVertexArrayObject>(new QOpenGLVertexArrayObject());
-    //构造顶点缓冲区
-    m_pVertexBuf = QSharedPointer<QOpenGLBuffer>(new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer));
-    m_pIndexBuf = QSharedPointer<QOpenGLBuffer>(new QOpenGLBuffer(QOpenGLBuffer::IndexBuffer));
-}
 
+}
 
 void FEModel::Commit()
 {
@@ -21,20 +17,22 @@ void FEModel::Commit()
     }
 
     //顶点数组绑定对象
+    m_vao = QSharedPointer<QOpenGLVertexArrayObject>(new QOpenGLVertexArrayObject());
     m_vao->create();
     m_vao->bind();
     // 完成初始化渲染数据 - 将CPU数据设置到GPU缓存中
+    //构造顶点缓冲区
+    m_pVertexBuf = QSharedPointer<QOpenGLBuffer>(new QOpenGLBuffer(QOpenGLBuffer::VertexBuffer));
     m_pVertexBuf->create();
-    m_pVertexBuf->bind();
+    m_pVertexBuf->bind();//此处可能失败
     m_pVertexBuf->setUsagePattern(QOpenGLBuffer::StaticDraw);
     m_pVertexBuf->allocate(verticesVect.constData(), verticesVect.size() * sizeof(FEVertex));
-    m_pVertexBuf->release();
-
+    //索引缓冲区
+    m_pIndexBuf = QSharedPointer<QOpenGLBuffer>(new QOpenGLBuffer(QOpenGLBuffer::IndexBuffer));
     m_pIndexBuf->create();
-    m_pIndexBuf->bind();
+    m_pIndexBuf->bind();//此处可能失败 需要做判断
     m_pIndexBuf->setUsagePattern(QOpenGLBuffer::StaticDraw);
     m_pIndexBuf->allocate(m_indexVect.constData(), m_indexVect.size() * sizeof(GLuint));
-    m_pIndexBuf->release();
 
     m_vao->release();//用完释放
 }
